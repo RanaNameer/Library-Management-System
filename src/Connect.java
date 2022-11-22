18347cc41dfba625b239622f99206ef192491734
Rana Nameer <rana.nameer.khan@gmail.com>
2022-11-22 17:43:36 +0500

    fixed connection to database

--- src/Connect.java
+++ src/Connect.java
@@ -6,7 +6,7 @@ public class Connect {
     public static Connection connect() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_conn?user=root&password=Naushair@21");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/LIBRARY", "root", "Naushair@21");
             return con;
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
